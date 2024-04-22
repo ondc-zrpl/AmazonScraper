41b3a71bb97122b17e8fa4b1c1bfb7dee839d06b
ondc-zrpl <140881492+ondc-zrpl@users.noreply.github.com>
2024-04-23 00:29:36 +0530

    UpdateNew app.py

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import requests
 
 app = Flask(__name__)
 
-def get_amazon_product_details(asin):
+
+def get_product_details(asin):
     ua = UserAgent()
     headers = {
         'User-Agent': ua.random,
@@ -29,6 +30,7 @@ def get_amazon_product_details(asin):
 
     # Product title
     product_title = soup.find('span', {'id': 'productTitle'}).text.strip()
+    print(product_title)
 
     # Product price
     price_element = soup.find('span', {'class': 'a-price-whole'})
@@ -36,9 +38,10 @@ def get_amazon_product_details(asin):
         product_price = price_element.text.strip()
         if product_price.endswith('.'):
             product_price = product_price[:-1]
+        
     else:
         product_price = 'Price not available'
-
+    print(product_price)
     # Check if product is sold by Amazon
     sold_by_amazon = bool(soup.find('span', {'id': 'sellerProfileTriggerId'}))
 
@@ -65,64 +68,21 @@ def get_amazon_product_details(asin):
         'sold_by_amazon': sold_by_amazon,
         'availability_status': availability_status
     }
-
-def get_flipkart_product_details(pid, lid):
-    ua = UserAgent()
-    headers = {
-        'User-Agent': ua.random,
-        'Accept-Language': 'en-US,en;q=0.5',
-        'Referer': 'https://www.google.com/',
-    }
-
-    print(f"Using User-Agent: {headers['User-Agent']}")
-
-    url = f'https://www.flipkart.com/brooks-launch-9-running-shoes-men/p/itmc1fa98405e951?pid={pid}&lid={lid}'
-    response = requests.get(url, headers=headers)
-
-    soup = BeautifulSoup(response.content, 'html.parser')
-
-    # Product name
-    product_name = soup.find('span', {'class': 'B_NuCI'}).text
-
-    # Product Price
-    price_tag = soup.find('div', {'class': '_30jeq3 _16Jk6d'})
-    product_price = '0'
-    if price_tag:
-        product_price = price_tag.text.replace('₹', '').replace(',', '')
-        product_price = float(product_price)
-
-    # Image URL
-    image_tag = soup.find('img', {'class': '_396cs4'})
-    image_url = ''
-    if image_tag and 'src' in image_tag.attrs:
-        image_url = image_tag['src']
-
-    return {
-        'title': product_name,
-        'price': product_price,
-        'image_url': image_url
-    }
-
+@app.route('/')
+def hello():
+    return 'Hello, World!'
 @app.route('/get_amazon_product_info', methods=['GET'])
-def get_amazon_product_info():
+def get_product_info():
     asin = request.args.get('asin')
+    
 
     if not asin:
         return jsonify(error='The "asin" parameter is required.'), 400
 
-    product_info = get_amazon_product_details(asin)
-    return jsonify(product_info)
+    
 
-@app.route('/get_flipkart_product_info', methods=['GET'])
-def get_flipkart_product_info():
-    pid = request.args.get('pid')
-    lid = request.args.get('lid')
-
-    if not pid or not lid:
-        return jsonify(error='Both "pid" and "lid" parameters are required.'), 400
-
-    product_info = get_flipkart_product_details(pid, lid)
+    product_info = get_product_details(asin)
     return jsonify(product_info)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
